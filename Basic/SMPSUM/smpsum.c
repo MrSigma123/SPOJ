f4893a816e27d5a10960c49b7be67b268f52cdb6
MrSigma123 <karol.czolpinski@gmail.com>
2024-08-26 15:16:39 +0200

    Add SMPSUM problem

---
+++ Basic/SMPSUM/smpsum.c
@@ -0,0 +1,30 @@
+/*
+SMPSUM - Iterated sums
+#basics
+
+Please compute the sum of squares for the given numbers: a, a+1, ..., b-1, b.
+
+Input
+
+Two numbers: a and b separated by space, where 1 <= a <= b <=100.
+
+Output
+
+Computed sum: a*a + (a+1)*(a+1) + ... + (b-1)*(b-1) + b*b
+
+Example
+
+Input:
+1 4
+
+Output:
+30
+Example 2
+
+Input:
+5 6
+
+Output:
+61
+*/
+

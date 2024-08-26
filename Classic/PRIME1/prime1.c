328a22c0e1e673f72481320b4774dcf92b45a16f
MrSigma123 <karol.czolpinski@gmail.com>
2024-08-26 15:52:14 +0200

    Format PRIME1 solution

--- Classic/PRIME1/prime1.c
+++ Classic/PRIME1/prime1.c
@@ -32,11 +32,10 @@ Warning: large Input/Output data, be careful with certain languages
 (though most should be OK if the algorithm is well designed)
 */
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
-int main (void){
+int main (void)
+{
   int test_cases;
-  // int max_value, min_value;
   int i = 0;
   int j = 0;
   int test_value, divider, checked_value, value_divider_flag, lower_value, higher_value, limit;
@@ -44,24 +43,28 @@ int main (void){
   // I can put all the max and min values into one one-dimensional array
   // (length 2*(max_value + min_value))
   int max_min_arr[test_cases*2];
-  while (i < test_cases*2){
+  while (i < test_cases*2)
+  {
     scanf("%d", &max_min_arr[i++]); // min value
     scanf("%d", &max_min_arr[i++]); // max value
   }
   printf("\n");
-  //printf("i == %d\n", i);
   // now array is filled with all test cases max an min values
   // it's the time to generate the prime number finder using
   // the data from just created array
-  i = 0; // for lower limit value index
-  value_divider_flag = 1; // assuming the checked value is prime
-  while (j < test_cases){ // for specific test cases
-    checked_value = max_min_arr[i++]; // minimal prime vale to start
-    divider = 2; // assign minimal test value
-    limit = max_min_arr[i++];   // assign max_value from range array
-    while (checked_value <= limit){
-      while (divider <= sqrt(checked_value)){
-        if (checked_value % divider == 0){
+  i = 0;                                // for lower limit value index
+  value_divider_flag = 1;               // assuming the checked value is prime
+  while (j < test_cases)
+  {               // for specific test cases
+    checked_value = max_min_arr[i++];   // minimal prime vale to start
+    divider = 2;                        // assign minimal test value
+    limit = max_min_arr[i++];           // assign max_value from range array
+    while (checked_value <= limit)
+    {
+      while (divider <= sqrt(checked_value))
+      {
+        if (checked_value % divider == 0)
+        {
           break;
         }
         divider++;

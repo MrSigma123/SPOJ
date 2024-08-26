21b498ad341f6b88b5fd996c9f1a2b4731f23392
MrSigma123 <karol.czolpinski@gmail.com>
2024-08-26 15:46:45 +0200

    Format TEST solution

--- Classic/TEST/test.c
+++ Classic/TEST/test.c
@@ -1,6 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
+/*
+TEST - Life, the Universe, and Everything
+#basic #tutorial #ad-hoc-1
 
+Your program is to use the brute-force approach in order to find the Answer to Life, the Universe, and Everything. More precisely... rewrite small numbers from input to output. Stop processing input after reading in the number 42. All numbers at input are integers of one or two digits.
+
+Example
+
+Input:
+1
+2
+88
+42
+99
+
+Output:
+1
+2
+88
+Information
+
+In case of any problems with your code, you can take a look in the forum, you'll find the answer, only for this problem, in various languages.
+*/
+#include <stdio.h>
 int main(void)
 {
 	int num;
@@ -18,6 +39,5 @@ int main(void)
 		    scanf("%d", &num);
 	    }
 	}
-	//printf("The answer is 42!\n");
 	return 0;
 }

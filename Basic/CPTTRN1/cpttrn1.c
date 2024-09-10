ba6aa85dd43e7bade536a0866052198bc014d1bd
MrSigma123 <karol.czolpinski@gmail.com>
2024-09-10 23:48:46 +0200

    Add CPTTRN1 solution

--- Basic/CPTTRN1/cpttrn1.c
+++ Basic/CPTTRN1/cpttrn1.c
@@ -38,3 +38,53 @@ Output:
 .*.*.
 
 */
+#include <stdio.h>
+void display_pattern(int size1, int size2);
+
+int main(void)
+{
+  int test_cases;
+  int i, j;
+  scanf("%d", &test_cases);
+  int pattern_size[100][2]; // to store up to 100 pattern sizes
+
+  for (i = 0; i < test_cases; i++) // enter the pattern sizes for all test cases
+  {
+    scanf("%d %d", &pattern_size[i][0], & pattern_size[i][1]);
+  }
+
+  for (i = 0; i < test_cases; i++) // display the patterns of the desired sizes
+  {
+    display_pattern(pattern_size[i][0], pattern_size[i][1]);
+    if (i != test_cases - 1)
+    {
+      printf("\n");
+    }
+  }
+  return 0;
+}
+void display_pattern(int size1, int size2)
+{
+  int i, j;
+  for (i = 0; i < size1; i++)
+  {
+    for (j = 0; j < size2; j++)
+    {
+      if (i % 2 == 0)
+      {
+        if (j % 2 == 0)
+          printf("*");
+        else
+          printf(".");
+      }
+      else
+      {
+        if (j % 2 == 0)
+          printf(".");
+        else
+          printf("*");
+      }
+    }
+    printf("\n");
+  }
+}

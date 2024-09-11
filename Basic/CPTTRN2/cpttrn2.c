e13fd7ccfaa108e007f2e51ed23fd443f3ca1041
MrSigma123 <karol.czolpinski@gmail.com>
2024-09-12 01:57:13 +0200

    Add CPTTRN2 solution

--- Basic/CPTTRN2/cpttrn2.c
+++ Basic/CPTTRN2/cpttrn2.c
@@ -37,3 +37,87 @@ Output:
 ***** 
 
 */
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+  int test_cases;
+  int ** pattern_sizes_arr; // pointer to a dynamically allocated 2D array
+  int size = 10;            // initial size for the number of cases
+  int i, j, k;
+  scanf("%d", &test_cases);
+
+  // allocate memory for the input array (array of pointers)
+  pattern_sizes_arr = (int **)malloc(size * sizeof(int *));
+
+  // check if the allocation succeeded
+  if (pattern_sizes_arr == NULL)
+  {
+    printf("Memory allocation failed!\n");
+    return 1;
+  }
+
+  // store input dynamically
+  for (i = 0; i < test_cases; i++)
+  {
+    // reallocate memory if the number of test cases exceeds the initial size
+    if (i >= size)
+    {
+      size *= 2;
+      pattern_sizes_arr = (int **)realloc(pattern_sizes_arr, size * sizeof(int *));
+    
+      if (pattern_sizes_arr == NULL)
+      {
+        printf("Memory allocation failed!\n");
+        return 1;
+      }
+    }
+
+    // allocate memory for each test case (each row has two integers)
+    pattern_sizes_arr[i] = (int *)malloc(2 * sizeof(int));
+
+    // check if the allocation succeed
+    if (pattern_sizes_arr == NULL)
+    {
+      printf("Memory allocation failed!\n");
+      return 1;
+    }
+
+    // read the input
+    scanf("%d %d", &pattern_sizes_arr[i][0], &pattern_sizes_arr[i][1]);
+  }
+
+  // print the results
+  for (i = 0; i < test_cases; i++)
+  {
+    for (j = 0; j < pattern_sizes_arr[i][0]; j++)
+    {
+      for (k = 0; k < pattern_sizes_arr[i][1]; k++)
+      {
+        if (j == 0 || j == pattern_sizes_arr[i][0] - 1
+            || k == 0 || k == pattern_sizes_arr[i][1] - 1)
+        {
+          printf("*");
+        }
+        else
+        {
+          printf(".");
+        }
+      }
+      printf("\n");
+    }
+    if (i != test_cases - 1)
+    {
+      printf("\n");
+    }
+  }
+
+  // free the allocated memory
+  for (i = 0; i < test_cases; i++)
+  {
+    free(pattern_sizes_arr[i]); // free each row
+  }
+  free(pattern_sizes_arr); // free the main array
+  
+  return 0;
+}

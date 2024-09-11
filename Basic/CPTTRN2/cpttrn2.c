c4cdcad3ce547bb61478379ae3b90d03957aba35
MrSigma123 <karol.czolpinski@gmail.com>
2024-09-11 23:52:35 +0200

    Add CPTTRN2 problem

---
+++ Basic/CPTTRN2/cpttrn2.c
@@ -0,0 +1,39 @@
+/*
+CPTTRN2 - Character Patterns (Act 2)
+#basics
+
+Using two characters: . (dot) and * (asterisk) print a frame-like pattern.
+
+Input
+
+You are given t - the number of test cases and for each of the test cases two
+positive integers: l - the number of lines and c - the number of columns of a
+frame.
+
+Output
+
+For each of the test cases output the requested pattern (please have a look at
+the example). Use one line break in between successive patterns.
+
+Example
+
+Input:
+3
+3 1
+4 4
+2 5
+
+Output:
+*
+*
+*
+
+****
+*..*
+*..*
+****
+
+*****
+***** 
+
+*/

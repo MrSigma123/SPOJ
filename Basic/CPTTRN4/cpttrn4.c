9ff22d5c76f9df89762681b5ad1dea51fd47d518
MrSigma123 <karol.czolpinski@gmail.com>
2024-09-12 23:07:38 +0200

    Add CPTTRN4 problem

---
+++ Basic/CPTTRN4/cpttrn4.c
@@ -0,0 +1,56 @@
+/*
+CPTTRN4 - Character Patterns (Act 4)
+#basics
+
+Using two characters: . (dot) and * (asterisk) print a grid-like pattern.
+
+Input
+
+You are given t - the number of test cases and for each of the test cases
+four positive integers: l - the number of lines, c - the number of columns
+in the grid; h and w - the high and the width of the single rectangle.
+
+Output
+
+For each of the test cases output the requested pattern (please have a look
+at the example). Use one line break in between successive patterns.
+
+Example
+
+Input:
+3
+3 1 2 1
+4 4 1 2
+2 5 2 2
+
+Output:
+***
+*.*
+*.*
+***
+*.*
+*.*
+***
+*.*
+*.*
+***
+
+*************
+*..*..*..*..*
+*************
+*..*..*..*..*
+*************
+*..*..*..*..*
+*************
+*..*..*..*..*
+*************
+
+****************
+*..*..*..*..*..*
+*..*..*..*..*..*
+****************
+*..*..*..*..*..*
+*..*..*..*..*..*
+****************
+
+*/

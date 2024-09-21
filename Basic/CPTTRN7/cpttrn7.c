11884eb446693c48b7d2d8a4178da25e7f188105
MrSigma123 <karol.czolpinski@gmail.com>
2024-09-21 15:46:58 +0200

    Fix pattern dimension

--- Basic/CPTTRN7/cpttrn7.c
+++ Basic/CPTTRN7/cpttrn7.c
@@ -2,15 +2,20 @@
 CPTTRN7 - Character Patterns (Act 7)
 #basics
 
-Print a regular grid pattern with diamond-like base elements. Use the \ (backslash) and the / (slash) characters to print diamonds and . (dots) to fill the rest of the space.
+Print a regular grid pattern with diamond-like base elements. Use the \
+(backslash) and the / (slash) characters to print diamonds and . (dots)
+to fill the rest of the space.
 
 Input
 
-You are given t - the number of test cases and for each of the test cases three positive integers: r - the number of rows, c - the number of columns in the grid and s - the size of each diamond.
+You are given t - the number of test cases and for each of the test cases
+three positive integers: r - the number of rows, c - the number of columns
+in the grid and s - the size of each diamond.
 
 Output
 
-For each of the test cases output the requested pattern (please have a look at the example). Use one line break in between successive patterns.
+For each of the test cases output the requested pattern (please have a look
+at the example). Use one line break in between successive patterns.
 
 Example
 
@@ -60,19 +65,18 @@ int main(void)
   int test_cases;
   int **pattern_sizes_arr;
   int size = 10; // initial size for the number of cases
-  int i, j, k, l, m, n;
+  int i, j, k, l, m;
   scanf("%d", &test_cases);
-
+  
   // allocate memory for the inpiut array (array of pointers)
   pattern_sizes_arr = (int **)malloc(size * sizeof(int *));
-
+  
   // check if the allocation succeeded
   if (pattern_sizes_arr == NULL)
   {
     printf("Memory allocation failed!\n");
     return 1;
   }
-
   // store the input dynamically
   for (i = 0; i < test_cases; i++)
   {
@@ -101,95 +105,74 @@ int main(void)
     scanf("%d %d %d", &pattern_sizes_arr[i][0], &pattern_sizes_arr[i][1],
           &pattern_sizes_arr[i][2]);
   }
-
   // print the results
-  for (i = 0; i < test_cases; i++) // for each case
+  for (i = 0; i < test_cases; i++) // for test cases
   {
-    for (j = 0; j < pattern_sizes_arr[i][0]; j++) // for each row
+    // we have to think by row oputputting
+    for (j = 0; j < pattern_sizes_arr[i][0]; j++) // for rows
     {
-      for (k = 0; k < pattern_sizes_arr[i][1]; k++) // for each column
+      for (k = 0; k < pattern_sizes_arr[i][2]; k++) // for first half of the pattern
       {
-        for (n = 0; n < pattern_sizes_arr[i][2]; n++) // for repeating U part of pattern line
+        for (l = 0; l < pattern_sizes_arr[i][1]; l++)
         {
-          for (l = 0; l < pattern_sizes_arr[i][2]; l++) // for diagonal UL part of pattern
+          for (m = 0; m < pattern_sizes_arr[i][2]; m++)
           {
-            for (m = 0; m < pattern_sizes_arr[i][2] / 2; m++)
+            if (l + m == pattern_sizes_arr[i][2] - 1)
             {
-              if (l + m + n == pattern_sizes_arr[i][2] - 1) 
-              {
-                printf("/");
-              }
-              else
-              {
-                printf(".");
-              }
+              printf("/");
+            }
+            else 
+            {
+              printf(".");
             }
           }
-          for (l = 0; l < pattern_sizes_arr[i][2]; l++) // for diagonal UR part of pattern
+          for (m = 0; m < pattern_sizes_arr[i][2]; m++)
           {
-            for (m = 0; m < pattern_sizes_arr[i][2] / 2; m++)
+            if (l == m)
             {
-              if (l - n == m)
-              {
-                printf("\\");
-              }
-              else
-              {
-                printf(".");
-              }
+              printf("\\");
+            }
+            else
+            {
+              printf(".");
             }
           }
-          printf("\n");
         }
-
+        printf("\n");
       }
-
-      for (k = 0; k < pattern_sizes_arr[i][1]; k++)
+    
+      for (k = 0; k <pattern_sizes_arr[i][2]; k++)
       {
-        for (n = 0; n < pattern_sizes_arr[i][2]; n++) // for repeating D part of pattern line
+        for (l = 0; l < pattern_sizes_arr[i][1]; l++)
         {
-          for (l = 0; l < pattern_sizes_arr[i][2]; l++) // for diagonal DL part of pattern
+          for (m = 0; m < pattern_sizes_arr[i][2]; m++)
           {
-            for (m = 0; m < pattern_sizes_arr[i][2] / 2; m++)
+            if (l == m)
             {
-              if (l - n == m)
-              {
-                printf("\\");
-              }
-              else
-              {
-                printf(".");
-              }
+              printf("\\");
+            }
+            else
+            {
+              printf(".");
             }
           }
-          for (l = 0; l < pattern_sizes_arr[i][2]; l++) // for diagonal DR part of pattern
+          for (m = 0; m < pattern_sizes_arr[i][2]; m++)
           {
-            for (m = 0; m < pattern_sizes_arr[i][2] / 2; m++)
+            if (l + m == pattern_sizes_arr[i][2] - 1)
             {
-              if (l + m + n == pattern_sizes_arr[i][2] - 1) 
-              {
-                printf("/");
-              }
-              else
-              {
-                printf(".");
-              }
+              printf("/");
+            }
+            else 
+            {
+              printf(".");
             }
           }
-          printf("\n");
         }
-
+        printf("\n");
       }
-
-    }
-
-    if (i < test_cases - 1)
-    {
-      
-      printf("\n");
     }
   }
-
+  
   // free the allocated memory
   for (i = 0; i < test_cases; i++)
   {

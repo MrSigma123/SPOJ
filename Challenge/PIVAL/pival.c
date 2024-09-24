2ccdf3e3f110cc1d8a6f8c45604169c533845238
MrSigma123 <karol.czolpinski@gmail.com>
2024-09-25 00:26:50 +0200

    Add PIVAL problem

---
+++ Challenge/PIVAL/pival.c
@@ -0,0 +1,21 @@
+/*
+PIVAL - Digits of Pi
+#big-numbers
+
+In this problem you have to find as many digits of PI as possible.
+
+Output
+
+Output must contain as many digits of PI as possible (not more than 1000000).
+
+Score
+
+The score awarded to your program will be the first position of the digit
+where the first difference occured.
+
+Example
+
+Output:
+3.1415926535897932384626433832795
+will be awarded with 33 points.
+*/

ac7e771283f997ef0ecc65c5af28d7fdf16df090
MrSigma123 <karol.czolpinski@gmail.com>
2024-10-06 14:19:29 +0200

    Reduce the file size

--- Classic/FCTRL2/fctrl2.c
+++ Classic/FCTRL2/fctrl2.c
@@ -1,90 +1,44 @@
-/*
-FCTRL2 - Small factorials
-#math #big-numbers
-
-You are asked to calculate factorials of some small positive integers.
-
-Input
-
-An integer t, 1 ≤ t ≤ 100, denoting the number of testcases, followed by
-t lines, each containing a single integer n, 1 ≤ n ≤ 100.
-
-Output
-
-For each integer n given at input, display a line with the value of n!
-
-Example
-
-Input:
-4
-1
-2
-5
-3
-
-Output:
-1
-2
-120
-6
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
-
 #define MAX 500  // Maximum digits for large factorials (e.g., 100!)
-
 // Function prototypes
 void input_values(int * destination, int size);
 void multiply(int x, int res[], int *res_size);
 void calculate_factorial(int n, char result[]);
 void print_results(int * array, int size);
-
 int main(void) {
     int test_cases;
     int * values;
-
     scanf("%d", &test_cases);
-
     // Allocate memory to store the input values
     values = (int *)malloc(test_cases * sizeof(int));
-
     // Check memory allocation
     if (values == NULL) {
         printf("Memory allocation failed!\n");
         return 1;
     }
-
     // Store input values
     input_values(values, test_cases);
-
     printf("\n"); // Separate input from output
-
     // Print the results after all input is processed
     print_results(values, test_cases);
-
     free(values);
-
     return 0;
 }
-
 // Function to input all test case values
 void input_values(int * destination, int size) {
     for (int i = 0; i < size; i++) {
         scanf("%d", &destination[i]);
     }
 }
-
 // Function to calculate factorial of a number and store it in result as a string
 void calculate_factorial(int n, char result[]) {
     int res[MAX];
     res[0] = 1;
     int res_size = 1;
-
     for (int x = 2; x <= n; x++) {
         multiply(x, res, &res_size);
     }
-
     // Convert the result array (digits) into a string
     int index = 0;
     for (int i = res_size - 1; i >= 0; i--) {
@@ -92,7 +46,6 @@ void calculate_factorial(int n, char result[]) {
     }
     result[index] = '\0';  // Null-terminate the string
 }
-
 // Function to multiply a large number by an integer x
 void multiply(int x, int res[], int *res_size) {
     int carry = 0;
@@ -102,13 +55,11 @@ void multiply(int x, int res[], int *res_size) {
         res[i] = product % 10;
         carry = product / 10;
     }
-
     while (carry) {
         res[(*res_size)++] = carry % 10;
         carry /= 10;
     }
 }
-
 // Function to print the factorial results
 void print_results(int * array, int size) {
     char result[MAX];
@@ -118,4 +69,3 @@ void print_results(int * array, int size) {
         printf("%s\n", result);
     }
 }
-

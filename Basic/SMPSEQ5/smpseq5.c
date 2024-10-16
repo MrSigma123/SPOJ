d5cc553199e07344bc5ac295ef495b8351f74b88
MrSigma123 <karol.czolpinski@gmail.com>
2024-10-16 23:47:09 +0200

    Add SMPSEQ5 problem

---
+++ Basic/SMPSEQ5/smpseq5.c
@@ -0,0 +1,44 @@
+/*
+SMPSEQ5 - Fun with Sequences (Act 3)
+#simple-math #basics
+
+You are given a sequence of n integers S = s1, s2 ... sn and a sequence of m
+integers Q = q1, q2 ... qm.
+
+Please print in ascending order all such i, that si = qi, i<=n, i<=m.
+
+Input data specification
+
+In the first line you are given one integer 2 <= n <= 100, and in the
+following line n integers:
+-100 <= si <= 100, si <= si+1.
+
+In the third line you are given one integer 2 <= m <= 100, and in the
+following line m integers:
+-100 <= qi <= 100, qi <= qi+1.
+
+Output data specification
+
+The sequence of requested indexes separated by spaces.
+
+Example 1
+
+Input:
+5
+-2 -2 -1 1 4 
+6
+-3 -2 -1 1 2 3
+
+Output:
+2 3 4
+Example 2
+
+Input:
+5
+-2 -1 -3 1 4 
+5
+-3 -2 -1 1 2
+
+Output:
+4
+*/

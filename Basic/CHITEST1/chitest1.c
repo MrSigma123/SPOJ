1960cee0b9a91898d033305cbe886c12c693ee82
MrSigma123 <karol.czolpinski@gmail.com>
2024-10-19 23:50:51 +0200

    Add CHITEST1 problem

---
+++ Basic/CHITEST1/chitest1.c
@@ -0,0 +1,31 @@
+/*
+CHITEST1 - Sum of two numbers
+no tags 
+
+Compute the sum of two numbers.
+
+Input
+
+The first line of the input will include the number N of test cases,
+and the each of the following N lines will include two numbers separated
+by whitespace.
+
+Output
+
+Starting with the second line of the input, the sum of the two numbers
+in the line must be printed into a line in the output. The accuracy of
+the output should be better than 10^-6.
+
+Example
+
+Input:
+3 
+2 3
+3.3 4
+-1 4
+
+Output:
+5
+7.3
+3
+*/

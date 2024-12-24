297b024740c1bb924ec6c61c52e5ba2779fc8b00
MrSigma123 <karol.czolpinski@gmail.com>
2024-12-25 00:01:30 +0100

    Add HMRO problem

--- Classic/HMRO/hmro.c
+++ Classic/HMRO/hmro.c
@@ -2,7 +2,19 @@
 HMRO - Help the Military Recruitment Office!
 no tags 
 
-At the end of year 2004, the regional agencies of the Polish Military Recruitment Office (known as WKU in Polish) is sending a call to all boys born in 1984. Every recruit has his personal 11-digit identification number (PESEL, format: YYMMDDXXXXX, where YYMMDD is the date of birth, and XXXXX is a zero-padded integer smaller than 100000). Every agency of the Military Recruitment Office has its own code (MRO, format: a place code consisting of 3 upper case letters and a one-digit number). But this year the army underwent some reforms and not all boys at conscription age are going to be recruited. The list of closed down MRO points is as follows: the code of the closed down MRO is followed by the code of some other MRO, to which all the recruits are now going to be assigned. The list of recruits contains their PESEL codes. Your task is to prepare the complete list of recruits and determine the codes of their new MRO-s.
+At the end of year 2004, the regional agencies of the Polish Military
+Recruitment Office (known as WKU in Polish) is sending a call to all
+boys born in 1984. Every recruit has his personal 11-digit identification
+number (PESEL, format: YYMMDDXXXXX, where YYMMDD is the date of birth,
+and XXXXX is a zero-padded integer smaller than 100000). Every agency of
+the Military Recruitment Office has its own code (MRO, format: a place code
+consisting of 3 upper case letters and a one-digit number). But this year
+the army underwent some reforms and not all boys at conscription age are
+going to be recruited. The list of closed down MRO points is as follows:
+the code of the closed down MRO is followed by the code of some other MRO,
+to which all the recruits are now going to be assigned. The list of recruits
+contains their PESEL codes. Your task is to prepare the complete list of
+recruits and determine the codes of their new MRO-s.
 
 Input
 
